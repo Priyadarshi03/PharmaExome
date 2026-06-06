7d938ddd9996700e140224062ac93cf5b4a36079
Priyadarshi03 <priyadarshianand03@gmail.com>
2026-06-06 09:50:17 +0530

    updated repository

--- scripts/parse_vcf.py
+++ scripts/parse_vcf.py
@@ -1,24 +1,21 @@
-import sys
-import gzip
 import pandas as pd
+import sys
 
 vcf = sys.argv[1]
-out = sys.argv[2]
+bed = sys.argv[2]
+out = sys.argv[3]
 
-pgx_genes = {
-    "CYP2C19",
-    "CYP2D6",
-    "CYP2C9",
-    "CYP3A5",
-    "VKORC1",
-    "TPMT",
-    "NUDT15",
-    "SLCO1B1"
-}
+genes = pd.read_csv(
+    bed,
+    sep="\t",
+    header=None,
+    names=["chrom","start","end","gene"]
+)
 
 rows = []
 
-with gzip.open(vcf, "rt") as fh:
+with open(vcf) as fh:
+
     for line in fh:
 
         if line.startswith("#"):
@@ -27,19 +24,28 @@ with gzip.open(vcf, "rt") as fh:
         cols = line.strip().split("\t")
 
         chrom = cols[0]
-        pos = cols[1]
-        ref = cols[3]
-        alt = cols[4]
+        pos = int(cols[1])
 
         gene = "UNKNOWN"
 
-        rows.append(
-            [chrom, pos, ref, alt, gene]
-        )
+        hits = genes[
+            (genes.chrom == chrom) &
+            (genes.start <= pos) &
+            (genes.end >= pos)
+        ]
 
-df = pd.DataFrame(
+        if not hits.empty:
+            gene = hits.iloc[0]["gene"]
+
+        rows.append([
+            chrom,
+            pos,
+            cols[3],
+            cols[4],
+            gene
+        ])
+
+pd.DataFrame(
     rows,
-    columns=["CHROM", "POS", "REF", "ALT", "GENE"]
-)
-
-df.to_csv(out, sep="\t", index=False)
+    columns=["CHROM","POS","REF","ALT","GENE"]
+).to_csv(out, sep="\t", index=False)
